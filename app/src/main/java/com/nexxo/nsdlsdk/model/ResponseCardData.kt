f430b8a73015c6b7a76b7b77a07f37a546b327ef
Ashutosh Singh <ashutosh.singh@nexxo.com>
2023-03-17 18:45:57 +0530

    saving account card added

--- app/src/main/java/com/nexxo/nsdlsdk/model/ResponseCardData.kt
+++ app/src/main/java/com/nexxo/nsdlsdk/model/ResponseCardData.kt
@@ -5,6 +5,6 @@ import com.google.gson.annotations.SerializedName
 
 data class ResponseCardData(
     @SerializedName("CustomerData" ) var CustomerData : ArrayList<CustomerData> = arrayListOf(),
-    @SerializedName("response"     ) var response     : String?                 = null,
-    @SerializedName("respcode"     ) var respcode     : String?                 = null
+    @SerializedName("response"     ) var response     : String?                 = "",
+    @SerializedName("respcode"     ) var respcode     : String?                 = ""
 )
